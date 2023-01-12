50da5341382f2cc1472fb06b45143af3377d9e32
Connor Anderson <connor.anderson@byu.edu>
2023-01-12 16:01:23 -0700

    Add install_requiremnts.sh

---
+++ install_requirements.sh
@@ -0,0 +1,17 @@
+# It's highly recommended to install these inside a virtual environment
+
+pip install fgvcdata
+pip install hydra-colorlog
+pip install hydra-core>=1.1.1
+pip install numba>=0.53.1
+pip install numpy>=1.20.2
+pip install omegaconf>=2.1.0
+pip install opencv-python>=4.5.1.48
+pip install Pillow>=8.1.0
+pip install tqdm 
+pip install python-dotenv
+
+# Choose the appropriate version of pytorch for your system
+# Here we're using a preview version with cuda 11
+pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu116
+pip install pytorch-lightning>=1.4.0 torchmetrics>=0.4.1 segmentation-models-pytorch
